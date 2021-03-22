4399828056aab00be4a938c95bfef89e51439ae2
Cesar <design@eight87.com>
2021-03-22 10:27:23 +0100

    My orders View

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -48,73 +48,9 @@ class Admin::ProductsController < Admin::ApplicationController
   end
 
   def approve
-    new_product = ShopifyAPI::Product.find @product.shopify_product_id rescue nil
-    new_product = ShopifyAPI::Product.new if new_product.nil?
-    new_product.title = @product.name
-    new_product.body_html = @product.description
-    new_product.product_type = "Plato"
-    new_product.vendor = @product.user.vendor
-    new_product.save
-    new_product.variants[0].price = @product.price
-    new_product.variants[0].sku = @product.available_quantity
-    # new_product.variants[0].inventory_quantity = @product.available_quantity
-    new_product.variants[0].weight = @product.weight
-
-    if new_product.save!
-
-      @product.update(shopify_product_id: new_product.id)
-
-      # inventory item
-      inventory_item = ShopifyAPI::InventoryItem.find new_product.variants[0].inventory_item_id rescue nil
-      if inventory_item
-        inventory_item.tracked = true
-        inventory_item.save!
-      end
-
-      if @product.image_base64.present?
-        i = ShopifyAPI::Image.new
-        i.attach_image(Base64.decode64(@product.image_base64.gsub("data:image/png;base64,", ""))) # <-- attach_image is a method, not an attribute
-        new_product.images << i
-
-        new_product.save
-      end
-
-      # ingredients
-      if @product.ingredients.present?
-        new_product.add_metafield(ShopifyAPI::Metafield.new({
-          namespace: 'come',
-          key: 'ingredientes',
-          value: @product.ingredients,
-          value_type: 'string'
-        }))
-      end
-
-      # disponibleDias
-      new_product.add_metafield(ShopifyAPI::Metafield.new({
-        namespace: 'come',
-        key: 'disponibleDias',
-        value: @product.delivery_days.gsub(", ", "|"),
-        value_type: 'string'
-      }))
-
-      # disponibleFranjas
-      new_product.add_metafield(ShopifyAPI::Metafield.new({
-        namespace: 'come',
-        key: 'disponibleFranjas',
-        value: @product.delivery_hours.gsub(", ", "|"),
-        value_type: 'string'
-      }))
-
-      @product.product_collections.each do |product_collection|
-        collection = ShopifyAPI::CustomCollection.find(product_collection.collection_id)
-        collection.add_product new_product
-      end
-      @product.update(status: true)
-      redirect_to admin_products_path, notice: 'Product is approved'
-    else
-      redirect_to admin_products_path, alert: 'Product is not approved'
-    end
-
+    update_on_shopify
+    @product.update(status: true)
+    redirect_to admin_products_path, notice: 'Product is approved'
   end
 
   private
@@ -151,7 +87,11 @@ class Admin::ProductsController < Admin::ApplicationController
         inventory_item.save
       end
 
+      # images
       if @product.image_base64.present?
+        new_product.images.each do |image|
+          image.destroy
+        end
         i = ShopifyAPI::Image.new
         i.attach_image(Base64.decode64(@product.image_base64.gsub("data:image/png;base64,", ""))) # <-- attach_image is a method, not an attribute
         new_product.images << i
@@ -189,10 +129,6 @@ class Admin::ProductsController < Admin::ApplicationController
         collection = ShopifyAPI::CustomCollection.find(product_collection.collection_id)
         collection.add_product new_product
       end
-      # @product.update(status: true)
-      # redirect_to admin_products_path, notice: 'Product is approved'
-    # else
-      # redirect_to admin_products_path, alert: 'Product is not approved'
     end
   end
 
